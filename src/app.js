60c8c6477086b93cdc81684f783482a2c4af8ca0
Dinesh Singh <dineshs@securepays.co.in>
2026-03-18 21:51:50 +0530

    ##S2E4 - Routing and Request Handlers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res)=> {
-    res.send("Hello World")
-})
 
-app.use("/about", (req, res)=> {
-    res.send("Hello World")
-})
 
 app.listen(4000, ()=>{
     console.log("server is runnning")
