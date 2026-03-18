46666996dfad9c4d23cb76d4916be814f884797d
Dinesh Singh <dineshs@securepays.co.in>
2026-03-18 17:45:23 +0530

    s2e3: creting our server

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res)=> {
+app.use("/", (req, res)=> {
+    res.send("Hello World")
+})
+
+app.use("/about", (req, res)=> {
     res.send("Hello World")
 })
 
