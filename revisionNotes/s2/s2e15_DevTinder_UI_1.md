05c0b7f2da77630f3f336ce9662e357ac45faed0
Dinesh Singh <dineshs@securepays.co.in>
2026-04-01 12:16:57 +0530

    s2e14 feed api and pagination

---
+++ revisionNotes/s2/s2e15_DevTinder_UI_1.md
@@ -0,0 +1,4 @@
+## Episode-15 - DevTinder UI - Part 1 ✅
+
+
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -55,4 +55,66 @@ userRouter.get("/user/connections", UserAuth, async (req, res) => {
     res.status(400).send("Error: " + error.message);
   }
 });
+
+//feed
+
+// User should see all the cards except
+// 1. his own card
+// 2. his connections
+// 3. ignored people
+// 4. already sent the connection express.request
+
+// or
+
+//my version
+//1. do now show my profile in feed
+//2. do not show profile whom i already sent request of interested and ignored
+//3. do not show profile whom i already accepted (have connection) and rejected
+
+userRouter.get("/user/feed", UserAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    //to send a max of 50 user
+    limit = limit > 50 ? 50 : limit;
+    console.log("limit: ", limit);
+
+    const skip = (page - 1) * limit;
+
+    //finds all connection requests (sent + receieved)
+    //select - to filter or send specific data
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    //user who sent or receive req from/to me
+    //new Set only store unique values
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((conn) => {
+      hideUsersFromFeed.add(conn.fromUserId.toString());
+      hideUsersFromFeed.add(conn.toUserId.toString());
+    });
+
+    console.log("hideUsersFromFeed: ", hideUsersFromFeed);
+    const users = await User.find({
+      //$nin - not in array (id should not be present in Array from hideUsersFromFeed)
+      //$ne - not equal to
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({
+      message: "Data fetch successfully!",
+      data: users,
+    });
+  } catch (error) {
+    res.status(400).json("Error: " + error.message);
+  }
+});
 module.exports = userRouter;
